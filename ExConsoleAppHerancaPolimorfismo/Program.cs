1ae9e804607ff3d2101213ad42edb71716264a34
Igor Nascimento Medeiros <nascimentoigor49@gmail.com>
2021-04-15 20:30:15 -0300

    Um pequeno projeto de console utilizando herança e polimorfismo

---
+++ ExConsoleAppHerancaPolimorfismo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ExConsoleAppHerancaPolimorfismo.Entities;
+
+namespace ExConsoleAppHerancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            List<Employee> listEmployees = new List<Employee>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    listEmployees.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    listEmployees.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in listEmployees)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
